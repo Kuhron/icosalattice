1db70b164fa45947dc6fd0442cee1584de0b8f1d
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2024-11-27 21:09:07 +0000

    preparing for new release

--- scripts/PlotDataAtPointCodes.py
+++ scripts/PlotDataAtPointCodes.py
@@ -1,4 +1,4 @@
-import icosalattice.PlottingUtil as pu
+from icosalattice.PlotDataOnMap import plot_variable_interpolated_from_dict
 
 
 fp = "/home/kuhron/elegen/scripts/test_data.txt"
@@ -8,5 +8,6 @@ with open(fp) as f:
 lines = [x.strip().split(",") for x in lines]
 pcs, vals = zip(*lines)
 vals = [float(x) for x in vals]
+d = dict(zip(pcs, vals))
 
-print(pcs, vals)
+plot_variable_interpolated_from_dict(d, dots_per_degree=10)
